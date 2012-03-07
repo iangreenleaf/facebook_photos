7830ad644c65c3542692bef95fc7cf54a88d19a9
Ian Young <ian.greenleaf@gmail.com>
2012-03-07 15:54:02 -0800

    Handle no quotes, for Chrome

--- src.js
+++ src.js
@@ -4,7 +4,7 @@ thumbs = document.querySelectorAll(".uiMediaThumb i");
 var html = '';
 for (i=0; i < thumbs.length; i++) {
   var thumb = thumbs[i].style.backgroundImage;
-  var url = thumb.replace(/^url\("(.*)"\)/,"$1");
+  var url = thumb.replace(/^url\("?(.*[^"])"?\)/,"$1");
   var good_url = url.replace(/_a\.jpg/, "_n.jpg");
   html = html + '<img src="' + good_url + '" />';
 }
